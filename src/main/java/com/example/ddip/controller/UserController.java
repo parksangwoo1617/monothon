516aeecb29d5e9d4e8e38582238946c05771d68a
parksangwoo <b01056721617@gmail.com>
2022-01-09 02:52:34 +0900

    awkje

--- src/main/java/com/example/ddip/controller/UserController.java
+++ src/main/java/com/example/ddip/controller/UserController.java
@@ -1,16 +1,14 @@
 package com.example.ddip.controller;
 
 import com.example.ddip.dto.request.LoginRequest;
+import com.example.ddip.dto.request.RegisterGoodsRequest;
 import com.example.ddip.dto.request.SignUpRequest;
 import com.example.ddip.dto.response.TokenResponse;
 import com.example.ddip.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -29,6 +27,11 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest request) {
         return userService.login(request);
+    }
+
+    @PostMapping("/goods")
+    public void registerGoods(@ModelAttribute RegisterGoodsRequest request) {
+        userService.registerGoods(request);
     }
 
 }
